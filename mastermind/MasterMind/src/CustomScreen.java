262b6a9f70f31391da7ad55c2508482180960e44
OtoshigamiX <stridersxv@gmail.com>
2015-04-21 22:14:21 +0200

    Revert "Revert "text box""
    This reverts commit 13f33430c88c58a7392286dddafea830fc9e3e1e.

---
+++ mastermind/MasterMind/src/CustomScreen.java
@@ -0,0 +1,10 @@
+import com.googlecode.lanterna.gui.Window;
+import com.googlecode.lanterna.gui.component.Panel;
+
+
+public class CustomScreen extends Window {
+	CustomScreen(){
+		super("Specjalne opcje");
+		Panel mainPanel = new Panel(Panel.Orientation.VERTICAL);
+	}
+}
